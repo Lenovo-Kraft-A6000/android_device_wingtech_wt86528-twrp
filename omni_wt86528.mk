e547baf103b1ea4e855649243967ffee92e1c6a8
TingyiChen <tingyi364@gmail.com>
2016-10-16 06:31:13 +0800

    Update and rename omni_wt86518.mk to omni_wt86528.mk

--- omni_wt86528.mk
+++ omni_wt86528.mk
@@ -15,9 +15,9 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-$(call inherit-product, device/lenovo/wt86518/full_wt86518.mk)
+$(call inherit-product, device/wingtech/wt86528/full_wt86528.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_NAME := omni_wt86518
+PRODUCT_NAME := omni_wt86528
