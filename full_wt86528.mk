7803ac8c94846dccde43278cf1b28d4f50be117d
TingyiChen <tingyi364@gmail.com>
2016-10-16 06:30:38 +0800

    Update and rename full_wt86518.mk to full_wt86528.mk

--- full_wt86528.mk
+++ full_wt86528.mk
@@ -35,8 +35,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.qcom:root/fstab.qcom
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := wt86518
-PRODUCT_NAME := full_wt86518
+PRODUCT_DEVICE := wt86528
+PRODUCT_NAME := full_wt86528
 PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := K30-T
+PRODUCT_MODEL := K31-t3
 PRODUCT_MANUFACTURER := Lenovo
