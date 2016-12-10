e34f05031606bbd4fc3c918e557ce2a275b3200c
TingyiChen <tingyi364@gmail.com>
2016-12-10 21:03:15 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 # Product-specific compile-time definitions.
 #
 
-LOCAL_PATH := device/lenovo/wt86528
+LOCAL_PATH := device/wingtech/wt86528
 
 TARGET_BOARD_VENDOR := lenovo
 
@@ -25,34 +25,39 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
 # Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8916
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
 
 # Kernel
-TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_HEADER_ARCH := arm
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
-#TARGET_USES_UNCOMPRESSED_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androideabi-
+TARGET_USES_UNCOMPRESSED_KERNEL := true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x80000100
-BOARD_RAMDISK_OFFSET     := 0x81000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/recovery/kernel
+#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 
-#If enable this,using builting kernel from source instead of prebuilt kernel
-#TARGET_KERNEL_SOURCE := kernel/lenovo/msm8916
-#ARGET_KERNEL_CONFIG := wt86528-perf_defconfig
+TARGET_KERNEL_SOURCE := kernel/wingtech/wt86528
+ARGET_KERNEL_CONFIG := wt86528-perf_defconfig
 
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
@@ -68,7 +73,7 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1887436800
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5132779520
 
 #Flags
-COMMON_GLOBAL_CFLAGS  := -DWT_USE_FAN54015
+#COMMON_GLOBAL_CFLAGS  := -DWT_USE_FAN54015 //Cause Can't compile with this flag
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/twrp.fstab
